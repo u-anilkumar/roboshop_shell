d775524b3e4a4dab726c693029f262cbd5589108
anilkummar <uanilchoudary98@gmail.com>
2026-03-20 14:42:21 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+Curr_Dir=$PWD
+
+echo "$Curr_Dir"
 #enable colours
 R='\e[31m'
 G='\e[32m'
@@ -8,10 +11,20 @@ Y='\e[33m'
 #check if root user
 USER_ID=$(id -u)
 
+validate()
+{
+    if [ $1 -ne 0 ]; then 
+    echo -e " $2... $R FAILURE $N"
+    else 
+    echo -e " $2... $G SUCCESS $N"
+}
+
+echo -e "user id is $G $USER_ID $N"
 if [ $USER_ID -ne 0 ]; then
 
-echo -e "$R Please execute the $G script with$Y sudo $N access"
+echo -e "$R Please execute the script with sudo access $N"
 exit 1
-else 
-echo -e "$G user has root access"
 fi
+
+cp Curr_Dir/mongodb.repo /etc/yum.repos.d/mongo.repo
+validate $? "Copying repo file repo folder"
